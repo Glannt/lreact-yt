00535f5ba70dc96d397f54c9c97b81b1c7220699
glannt <tongnguyenthanhdo@gmail.com>
2024-08-26 14:58:11 +0700

    updateAddUser-processing

--- src/Content/ManageUser.js
+++ src/Content/ManageUser.js
@@ -1,16 +1,14 @@
 import ModalCreateUser from "./ModalCreateUser";
+import "./ManageUser.scss";
 const ManageUser = (props) => {
   return (
     <div className="manage-user-container">
       <div className="title">Manage User</div>
       <div className="users-content">
         <div>
-          <button>Add new User</button>
-        </div>
-        <div>
-          Table user
           <ModalCreateUser />
         </div>
+        <div>Table user</div>
       </div>
     </div>
   );
